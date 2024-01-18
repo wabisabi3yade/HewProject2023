a2cd12b9df1a86be68fd06fccc51866ff0f36f4d
WIN11OHS20218\barre <barrel.totoro@gmail,com>
2024-01-18 15:25:50 +0900

    値の渡しに成功

--- HewProject/HewProject/COperation.h
+++ HewProject/HewProject/COperation.h
@@ -3,19 +3,22 @@
 #include <Windows.h>
 #include <Xinput.h>
 #include "Vector3.h"
+#include "Vector2.h"
 
 class GameController
 {
+	// LRスティックの傾き（範囲：-1～1）
+	Vector2 stick_L = {};
+	Vector2 stick_R = {};
+
+	Vector2 deadZone_L = {};
 public:
 
-	//現在の向きを表すベクトル変数
+	/*//現在の向きを表すベクトル変数
 	Vector3 dir = { 0, 0, 0 };
 
 	//移動速度
 	float moveSpeed = 1.0f;
-
-	// キー操作可能なキャラクターか？
-	bool isPlayer = true;
 
 	//仮想世界の中の位置座標
 	Vector3 pos = { 0, 0, 0 };
@@ -24,17 +27,24 @@ public:
 	Vector3 scale = { 1.0f, 1.0f, 1.0f };
 
 	//回転角度
-	float angle = 0.0f;
+	float angle = 0.0f;*/
 
+	// キー操作可能なキャラクターか？
+	bool isPlayer = true;
 
 	GameController();
 
 	~GameController();
 
+	void GamePad();
+	/*Vector3 GamePad(float stickX, float stickY);*/
 
-	Vector3 GamePad();
+	// 左の傾きを取得する
+	Vector2 GetStick_L()const { return stick_L; }
 
-	void SetMoveSpeed(float sp)
+	void SetDeadZone_L_X(const float& _setDaeadZoneX) { deadZone_L.x = _setDaeadZoneX; }
+	void SetDeadZone_L_Y(const float& _setDaeadZoneY) { deadZone_L.y = _setDaeadZoneY; }
+	/*void SetMoveSpeed(float sp)
 	{
 		moveSpeed = sp;
 	}
@@ -42,18 +52,11 @@ public:
 	void SetDir(Vector3 v)
 	{
 		dir = v;
-	}
+	}*/
 
 
 private:
-	XINPUT_STATE m_controllerState;
 
 	// XInputの初期化
 	XINPUT_STATE controllerState;
-
-	// スティックの値を格納する変数
-	float m_leftStickX;
-	float m_leftStickY;
-	float m_rightStickX;
-	float m_rightStickY;
 };
--- HewProject/HewProject/CTatemizoScene.cpp
+++ HewProject/HewProject/CTatemizoScene.cpp
@@ -31,10 +31,104 @@ CTatemizoScene::~CTatemizoScene()
 void CTatemizoScene::Update()
 {
 
-	Vector3 G (x->GamePad());
+
+	if (isPlayer)
+	{
+		// 操作で設定する用のベクトル変数
+		//V/*ector3 d = { 0.0f,0.0f,0.0f };*/
+
+		dir = { 0,0,0 };
+
+		x->GamePad();
+
+		/*dir.x = x->GetStick_L().x;
+		dir.y = x->GetStick_L().y;*/
+
+		Vector2 a = x->GetStick_L();
+		
+		if (x->GetStick_L().x > 0)
+		{
+			// 右上
+			if (x->GetStick_L().y > 0)
+			{
+				dir = { 1,1,0 };
+			}
+
+			// 右下
+			if (x->GetStick_L().y < 0)
+			{
+				dir = { 1,-1,0 };
+			}
+
+		}
+
+		if (x->GetStick_L().x < 0)
+		{
+			// 左上			
+			if (x->GetStick_L().y > 0)
+			{
+				dir = { -1,1,0 };
+			}
+
+			// 左下
+			if (x->GetStick_L().y < 0)
+			{
+				dir = { -1,-1,0 };
+			}
+
+		}
 	
-	charObj->mTransform.pos.x += G.x*0.1;
-	charObj->mTransform.pos.y += G.y*0.1;
+
+
+		/*Vector3 G(x->GamePad(0.0, 0.0));*/
+
+
+			/*//右上
+			if ((stickX <= 1) && (stickX > 0.5) && (stickY >= 0.25))
+			{
+				d.y = 1;// 上方向ベクトル
+				moveSpeed = 0.01f;
+
+				d.x = 1;// 右方向ベクトル
+				moveSpeed = 0.01f;
+			}
+			//右下
+			if ((stickX <= 1) && (stickX > 0.25) && (stickY < -0.25))
+			{
+				d.y = -1;// 上方向ベクトル
+				moveSpeed = 0.01f;
+
+				d.x = 1;// 右方向ベクトル
+				moveSpeed = 0.01f;
+			}
+			//左下
+			if ((stickX < 0) && (stickY < 0))
+			{
+				d.y = -1;// 上方向ベクトル
+				moveSpeed = 0.01f;
+
+				d.x = -1;// 右方向ベクトル
+				moveSpeed = 0.01f;
+			}
+			//左上
+			if ((stickX < 0) && (stickY > 0.25))
+			{
+				d.y = 1;// 上方向ベクトル
+				moveSpeed = 0.01f;
+
+				d.x = -1;// 右方向ベクトル
+				moveSpeed = 0.01f;
+			}*/
+
+			// キー操作でベクトルが設定されていたらdirに代入する
+			/*if (d.x != 0.0f || d.y != 0.0f)
+			{
+				dir = d;
+			}*/
+
+			charObj->mTransform.pos.x += dir.x * 0.1;
+			charObj->mTransform.pos.y += dir.y * 0.1;
+	}
 
 }
 
