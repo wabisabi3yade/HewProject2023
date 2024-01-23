8d5fbc2e90ca4761dad4f8c4c5683870a4ccc76c
wabisabi3yade <137769472+wabisabi3yade@users.noreply.github.com>
2024-01-23 18:03:36 +0900

    画像差し替え

--- HewProject/HewProject/NowLoadingText.cpp
+++ HewProject/HewProject/NowLoadingText.cpp
@@ -2,9 +2,10 @@
 #include "LoadCharacter.h"
 #include "TextureFactory.h"
 
+#define TEXT_BASEPOSX (-0.2f)	// テキスト1文字目のX座標
 #define TEXT_SIN_BASEPOSY (-0.7f)	// サイン関数で動かす軸となるY座標
 #define TEXT_SPACE_X (0.09f)	//	文字間のスペース
-#define TEXT_PERIOD_SPACE	// ピリオド間のスペース
+#define TEXT_PERIOD_SPACE (0.02f)	// ピリオド間のスペース
 
 #define TEXT_U_NUM (3)	// テキスト画像のU
 #define TEXT_V_NUM (4)
@@ -28,15 +29,24 @@ NowLoadingText::NowLoadingText()
 	{
 		text[num] = new UI(buffer, texWork);
 
-		// 左端から文字の距離をとる
-		text[num]->mTransform.pos = textBeginPos;
-		text[num]->mTransform.pos.x += num * TEXT_SPACE_X;
-
-		/*if()*/
+		if (num < static_cast<int>(TEXT::period_1))
+		{
+			// 左端から文字の距離をとる
+			text[num]->mTransform.pos = textBeginPos;
+			text[num]->mTransform.pos.x += num * TEXT_SPACE_X;
+		}
+		else
+		{
+			const float& gPos = text[static_cast<int>(TEXT::g)]->mTransform.pos.x;
+			text[num]->mTransform.pos.x = gPos + (num - static_cast<int>(TEXT::g)) * TEXT_PERIOD_SPACE;
+		}
 
 		// 角度を変えて
 		degree[num] = rand() % 360;
 		text[num]->mTransform.pos.y += TEXT_AMPLITUDE * sin(DirectX::XMConvertToRadians(degree[num]));
+
+		// 画像のUVを設定
+		text[num]->SetUV((1.0f / TEXT_U_NUM) * (num % TEXT_U_NUM), (1.0f / TEXT_V_NUM) * (num / TEXT_U_NUM));
 
 		// 大きさ調整
 		switch (static_cast<TEXT>(num))
@@ -82,6 +92,7 @@ NowLoadingText::NowLoadingText()
 		case TEXT::period_2:
 		case TEXT::period_3:
 			textScale = { 0.04f, 0.08f };
+			text[num]->SetUV((1.0f / TEXT_U_NUM) * (10 % TEXT_U_NUM), (1.0f / TEXT_V_NUM) * (10 / TEXT_U_NUM));
 			break;
 
 		}
@@ -89,8 +100,7 @@ NowLoadingText::NowLoadingText()
 		text[num]->mTransform.scale = { textScale.x, textScale.y, 1.0f };
 		
 
-		// 画像のUVを設定
-		text[num]->SetUV((1.0f / TEXT_U_NUM) * (num % TEXT_U_NUM), (1.0f / TEXT_V_NUM) * (num / TEXT_U_NUM));
+		
 
 		text[num]->materialDiffuse = { 1.0f / 255 * 255, 1.0f / 255 * 158, 1.0f / 255 * 255,1.0f };
 	};
