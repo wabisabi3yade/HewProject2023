f3039a1f6c48444e788b9c908acc53fd35d11b29
wabisabi3yade <137769472+wabisabi3yade@users.noreply.github.com>
2024-01-23 16:26:34 +0900

    テキストボックスの画像差し替え

--- HewProject/HewProject/NowLoadingText.cpp
+++ HewProject/HewProject/NowLoadingText.cpp
@@ -3,7 +3,8 @@
 #include "TextureFactory.h"
 
 #define TEXT_SIN_BASEPOSY (-0.7f)	// サイン関数で動かす軸となるY座標
-#define TEXT_SPACE_X (0.09f)	//	文字間のスペース 
+#define TEXT_SPACE_X (0.09f)	//	文字間のスペース
+#define TEXT_PERIOD_SPACE	// ピリオド間のスペース
 
 #define TEXT_U_NUM (3)	// テキスト画像のU
 #define TEXT_V_NUM (4)
@@ -27,11 +28,21 @@ NowLoadingText::NowLoadingText()
 	{
 		text[num] = new UI(buffer, texWork);
 
+		// 左端から文字の距離をとる
+		text[num]->mTransform.pos = textBeginPos;
+		text[num]->mTransform.pos.x += num * TEXT_SPACE_X;
+
+		/*if()*/
+
+		// 角度を変えて
+		degree[num] = rand() % 360;
+		text[num]->mTransform.pos.y += TEXT_AMPLITUDE * sin(DirectX::XMConvertToRadians(degree[num]));
+
 		// 大きさ調整
 		switch (static_cast<TEXT>(num))
 		{
 		case TEXT::N:
-			textScale = {0.1f, 0.2f};
+			textScale = { 0.1f, 0.2f };
 			break;
 
 		case TEXT::o_1:
@@ -67,30 +78,27 @@ NowLoadingText::NowLoadingText()
 			textScale = { 0.09f, 0.18f };
 			break;
 
-		case TEXT::period:
-			textScale = { 0.1f, 0.2f };
+		case TEXT::period_1:
+		case TEXT::period_2:
+		case TEXT::period_3:
+			textScale = { 0.04f, 0.08f };
 			break;
+
 		}
 
-		// 左端から文字の距離をとる
-		text[num]->mTransform.pos = textBeginPos;
-		text[num]->mTransform.pos.x += num * TEXT_SPACE_X;
-		
-		// 角度を変えて
-		degree[num] = rand() % 360;
-		text[num]->mTransform.pos.y += TEXT_AMPLITUDE * sin(DirectX::XMConvertToRadians(degree[num]));
-
 		text[num]->mTransform.scale = { textScale.x, textScale.y, 1.0f };
+		
 
 		// 画像のUVを設定
 		text[num]->SetUV((1.0f / TEXT_U_NUM) * (num % TEXT_U_NUM), (1.0f / TEXT_V_NUM) * (num / TEXT_U_NUM));
 
-		text[num]->materialDiffuse = { 1.0f,0.0f,0.0f,1.0f };
+		text[num]->materialDiffuse = { 1.0f / 255 * 255, 1.0f / 255 * 158, 1.0f / 255 * 255,1.0f };
 	};
 
 	texWork = texFactory->Fetch(L"asset/UI/LoadCharacter.png");
 	character = new LoadCharacter(buffer, texWork);
 	character->mTransform.scale = { 0.2f, 0.4f,1.0f };
+	character->materialDiffuse = { 1.0f / 255 * 255, 1.0f / 255 * 158, 1.0f / 255 * 255,1.0f };
 }
 
 NowLoadingText::~NowLoadingText()
@@ -106,12 +114,12 @@ NowLoadingText::~NowLoadingText()
 }
 
 void NowLoadingText::Update()
-{	
+{
 	for (int i = 0; i < static_cast<int>(TEXT::NUM); i++)
 	{
 		// 角度を増やしていく
 		degree[i] += TEXT_MOVESPEED;
-		
+
 		// 上下に動かす
 		text[i]->mTransform.pos.y = TEXT_SIN_BASEPOSY + TEXT_AMPLITUDE * sin(DirectX::XMConvertToRadians(degree[i]));
 	}
