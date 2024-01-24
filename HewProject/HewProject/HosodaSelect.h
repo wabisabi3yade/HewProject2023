109d119a2fd31339b6a1e1c5cee04e7008e5c385
wabisabi3yade <137769472+wabisabi3yade@users.noreply.github.com>
2024-01-25 00:51:17 +0900

    更新

--- HewProject/HewProject/HosodaSelect.h
+++ HewProject/HewProject/HosodaSelect.h
@@ -1,15 +1,15 @@
-#pragma once
-#include "CScene.h"
-class HosodaSelect
-	: public CScene
-{
-public:
-	HosodaSelect();
-	~HosodaSelect();
-
-	void Update() override;
-
-	void LateUpdate() override;
-
-	void Draw() override;
-};
+//#pragma once
+//#include "CScene.h"
+//class HosodaSelect
+//	: public CScene
+//{
+//public:
+//	HosodaSelect();
+//	~HosodaSelect();
+//
+//	void Update() override;
+//
+//	void LateUpdate() override;
+//
+//	void Draw() override;
+//};
