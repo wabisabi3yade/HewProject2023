d58a38cfa927e4035080d1db4f9ece15027336ec
nama-greentea <r.basketball0603@gmail.com>
2024-02-12 17:20:41 +0900

    文字追加

--- HewProject/HewProject/CGameStart.h
+++ HewProject/HewProject/CGameStart.h
@@ -10,7 +10,13 @@ public:
     D3DBUFFER bgBuffer;
     D3DTEXTURE bgTexture;
 
+    D3DBUFFER textBuffer;
+    D3DTEXTURE textTexture;
+
     UI* Bg;
+    UI* Text;
+
+    bool isProtein;
 
 public:
     CGameStart();
