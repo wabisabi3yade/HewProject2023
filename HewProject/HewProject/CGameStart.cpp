4d91e2980d3098cd5a8e650ebb6d297146b12fbb
nama-greentea <r.basketball0603@gmail.com>
2024-02-12 23:29:16 +0900

    更新したよ

--- HewProject/HewProject/CGameStart.cpp
+++ HewProject/HewProject/CGameStart.cpp
@@ -10,8 +10,9 @@ CGameStart::CGameStart()
 	bgTexture = TextureFactory::GetInstance()->Fetch(L"asset/UI/FadeBlack.png");
 
 	Bg = new UI(bgBuffer, bgTexture);
+	Bg->MakeDotween();
 	Bg->SetColor({ 0,0,0 });
-	Bg->SetAlpha(0.5f);
+	Bg->SetAlpha(0);
 	Bg->mTransform.pos = { 0,0,0.1f };
 	Bg->mTransform.scale = { 16.0f,9.0f,1.0f };
 
@@ -20,11 +21,14 @@ CGameStart::CGameStart()
 
 	Text = new UI(textBuffer, textTexture);
 	Text->MakeDotween();
-	Text->mTransform.pos = { 8.0f,3.0f,0 };
+	Text->mTransform.pos = { 10.0f,3.0f,0 };
 	Text->mTransform.scale = { 4.0f,1.0f,1.0f };
 
-	Protein = new ProteinUI(2, false);
-	Protein->SetPosition({ 8.0f,0,0 });
+	nNumProtein = 3;
+	fProteinZ = 0.0f;
+
+	Protein = new ProteinUI(nNumProtein, false);
+	Protein->SetPosition({ 11.0f,0,fProteinZ });
 	Protein->SetScale({ 2,2});
 
 	isProtein = false;
@@ -47,28 +51,71 @@ void CGameStart::Update()
 	{
 		isProtein = true;
 
-		Text->dotween->DoMoveX(0, 0.4f);
-		Text->dotween->Join(0, 2.0f, DoTweenUI::FUNC::DELAY);
-		Text->dotween->Append({-10.0f,3.0f,0}, 0.5f, DoTweenUI::FUNC::EASE_INBACK);
-		Text->dotween->OnComplete([&]() {
-			Text->SetActive(false);
-			});
+		Bg->dotween->DoAlpha(0.5f, 0.2f);
+		Bg->dotween->OnComplete([&]() {
+			Text->dotween->DoEaseOutBack({ 0,3.0f,0 }, 0.4f);
+			Text->dotween->Join(0, 2.0f, DoTweenUI::FUNC::DELAY);
+			Text->dotween->Append({ -10.0f,3.0f,0 }, 0.3f, DoTweenUI::FUNC::EASE_INBACK);
+			Text->dotween->OnComplete([&]() {
+				Text->SetActive(false);
+				});
 
-		Protein->GetDotween()->DoMoveX(0, 0.8f);
-		Protein->GetDotween()->Append({ -10.0f,0,0 }, 0.8f, DoTweenUI::FUNC::DELAY);
-		Protein->GetDotween()->OnComplete([&]() {
-			Protein->AddProtein();
-			Protein->GetDotween()->DelayedCall(0.2f, [&]() {
-				Protein->AddProtein();
-				Protein->GetDotween()->DelayedCall(0.8f, [&]() {
-					Protein->GetDotween()->DoEaseInBack({ -10.0f,0,0 }, 0.5f);
-					Protein->GetDotween()->DelayedCall(0.8f, [&]() {
-						Bg->SetActive(false);
+			if (nNumProtein == 1)
+			{
+				Protein->GetDotween()->DoEaseOutBack(0, 0.8f);
+				Protein->GetDotween()->Append({ -10.0f,0,0 }, 0.3f, DoTweenUI::FUNC::DELAY);
+				Protein->GetDotween()->OnComplete([&]() {
+					Protein->AddProtein();
+					Protein->GetDotween()->DelayedCall(0.9f, [&]() {
+						Protein->GetDotween()->DoEaseInBack({ -10.0f,0,0 }, 0.5f);
+						Protein->GetDotween()->OnComplete([&]() {
+							Bg->dotween->DoAlpha(0, 0.2f);
+							});
 						});
 					});
-				});
-			
+			}
+			else if (nNumProtein == 2)
+			{
+				Protein->GetDotween()->DoEaseOutBack(0, 0.8f);
+				Protein->GetDotween()->Append({ -10.0f,0,0 }, 0.1f, DoTweenUI::FUNC::DELAY);
+				Protein->GetDotween()->OnComplete([&]() {
+					Protein->AddProtein();
+					Protein->GetDotween()->DelayedCall(0.1f, [&]() {
+						Protein->AddProtein();
+						Protein->GetDotween()->DelayedCall(1.0f, [&]() {
+							Protein->GetDotween()->DoEaseInBack({ -10.0f,0,0 }, 0.5f);
+							Protein->GetDotween()->OnComplete([&]() {
+								Bg->dotween->DoAlpha(0, 0.2f);
+								});
+							});
+						});
+
+					});
+			}
+			else if (nNumProtein == 3)
+			{
+				Protein->GetDotween()->DoEaseOutBack(0, 0.8f);
+				Protein->GetDotween()->Append({ -10.0f,0,0 }, 0.1f, DoTweenUI::FUNC::DELAY);
+				Protein->GetDotween()->OnComplete([&]() {
+					Protein->AddProtein();
+					Protein->GetDotween()->DelayedCall(0.1f, [&]() {
+						Protein->AddProtein();
+						Protein->GetDotween()->DelayedCall(0.1f, [&]() {
+							Protein->AddProtein();
+							Protein->GetDotween()->DelayedCall(1.0f, [&]() {
+								Protein->GetDotween()->DoEaseInBack({ -12.0f,0,0 }, 0.5f);
+								Protein->GetDotween()->OnComplete([&]() {
+									Bg->dotween->DoAlpha(0, 0.2f);
+									});
+								});
+							});
+						});
+					});
+			}
 			});
+
+		
+		
 		
 	}
 	
--- HewProject/HewProject/CGameStart.h
+++ HewProject/HewProject/CGameStart.h
@@ -19,6 +19,8 @@ public:
     ProteinUI* Protein;
 
     bool isProtein;
+    int nNumProtein;
+    float fProteinZ;
 
 public:
     CGameStart();
